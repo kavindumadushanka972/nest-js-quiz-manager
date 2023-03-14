92f744d12617774001134c441341de3024a1b387
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2023-03-14 12:50:11 +0530

    feat:get all quizes

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Question } from 'src/question/entity/question.entity';
 import { Repository } from 'typeorm';
 import { CreateQuizeDto } from './dto/create-quiz.dto';
 import { Quiz } from './entity/quiz.entity';
 
 @Injectable()
 export class QuizService {
-
   constructor(
     @InjectRepository(Quiz)
-    private quizRepository: Repository<Quiz>
+    private quizRepository: Repository<Quiz>,
   ) {}
 
-  getAllQuiz() {
-    return [1,2,3];
+  async getAllQuiz(): Promise<Quiz []> {
+    return this.quizRepository
+      .createQueryBuilder('q')
+      .leftJoinAndSelect('q.questions', 'qt')
+      .leftJoinAndSelect('qt.options', 'o')
+      .getMany();
   }
 
   async getQuizById(id: number): Promise<Quiz> {
-    return await this.quizRepository.findOne({where: {id: id}, relations: ['questions']});
+    return await this.quizRepository.findOne({
+      where: { id: id },
+      relations: ['questions'],
+    });
   }
 
   async createNewQuiz(quiz: CreateQuizeDto): Promise<Quiz> {
